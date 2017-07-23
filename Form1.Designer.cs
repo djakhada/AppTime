68e2b9b954cbf82e2321c96c32b372ada4902006
Sinner <sinn3r@live.de>
2017-07-23 14:09:41 +0200

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace AppTime
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.databaseAvailableLabel = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timeTrackTimer = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.automaticUpdateTimer = new System.Windows.Forms.Timer(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add new program";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Current number of tracked programs: 0";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(3, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 37);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Create new database";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // databaseAvailableLabel
+            // 
+            this.databaseAvailableLabel.AutoSize = true;
+            this.databaseAvailableLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.databaseAvailableLabel.ForeColor = System.Drawing.Color.Red;
+            this.databaseAvailableLabel.Location = new System.Drawing.Point(235, 20);
+            this.databaseAvailableLabel.Name = "databaseAvailableLabel";
+            this.databaseAvailableLabel.Size = new System.Drawing.Size(181, 18);
+            this.databaseAvailableLabel.TabIndex = 4;
+            this.databaseAvailableLabel.Text = "No specified database yet.";
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Location = new System.Drawing.Point(15, 100);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(563, 348);
+            this.panel1.TabIndex = 5;
+            // 
+            // timeTrackTimer
+            // 
+            this.timeTrackTimer.Enabled = true;
+            this.timeTrackTimer.Interval = 60000;
+            this.timeTrackTimer.Tick += new System.EventHandler(this.timeTrackTimer_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(3, 46);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 37);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Update database";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label2.Location = new System.Drawing.Point(308, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(227, 26);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Automatic update frequency: Every 60 minutes\r\nDatabase gets saved on program exit" +
+    ".";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(264, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Last update: None since the program has been started";
+            // 
+            // automaticUpdateTimer
+            // 
+            this.automaticUpdateTimer.Enabled = true;
+            this.automaticUpdateTimer.Interval = 3600000;
+            this.automaticUpdateTimer.Tick += new System.EventHandler(this.automaticUpdateTimer_Tick);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(482, 16);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(96, 37);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Settings";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.checkBox1);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Location = new System.Drawing.Point(585, 13);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(139, 435);
+            this.panel2.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(3, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 18);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Settings";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(6, 117);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(82, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Show paths";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(25, 402);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 26);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "programmed by\r\ngithub.com/sinnzr";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(736, 460);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.databaseAvailableLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "AppTime";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label databaseAvailableLabel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timeTrackTimer;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer automaticUpdateTimer;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,389 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//#pragma warning disable 0169, 0414
+
+namespace AppTime
+{
+    class TrackedProgram
+    {
+        public int ID;
+        public string Path;
+        public string Name;
+        public int runtime = 0; //in minutes
+        public DateTime lastTimeRun;// = new DateTime(2017, 1, 1, 1, 1, 1, 1);
+        public DateTime timeAdded = DateTime.Now;// = DateTime.Now;
+        public TrackedProgram(string selectedPath = "None Given", string selectedName = "None Given", int selectedRuntime = 0, DateTime? selectedTimeAdded = null, DateTime? selectedLastTimeRun = null)
+        {
+            Path = selectedPath;
+            Name = selectedName;
+            selectedRuntime = runtime;
+            selectedLastTimeRun = lastTimeRun;
+            selectedTimeAdded = timeAdded;
+        }
+
+    }
+
+    public partial class Form1 : Form
+    {
+        List<TrackedProgram> Programs = new List<TrackedProgram>();
+        int numPrograms = 0;
+        //Label Programlist = new Label();
+
+        public SQLiteConnection dbConnection;
+        string dbLoc;
+        bool dbConnected = false;
+        bool dbExist = false;
+
+        
+        //settings        
+        bool settingsPanelExtended;
+        bool showPaths;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+            
+            //Registry
+            using (RegistryKey Key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\sinnzrAppTime\"))
+                if (Key != null)
+                {
+                    dbLoc = (string)Key.GetValue("databaseLocation");
+                    if (File.Exists(dbLoc))
+                    {
+                        dbExist = true;
+                        connectDb(false, false);
+                        databaseAvailableLabel.Visible = false;
+                        loadDatabase();
+                    }
+                }
+            if (!dbExist)
+            {
+                panel2.Enabled = true;
+                this.Size = new Size(752, 499); //with sidepanel
+                settingsPanelExtended = true;
+            }
+            else this.Size = new Size(601, 499);settingsPanelExtended = false;
+
+            loadSettings();
+            timeTrackTimer.Start();
+            automaticUpdateTimer.Start();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (dbConnected)
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                ofd.Filter = "Executable Files (*.exe)|*.exe";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    AddProgram(ofd.FileName, ofd.SafeFileName);
+                }
+            }
+            else MessageBox.Show("Create a databank first.");
+        }
+
+        private void updateLabel()
+        {
+            for (int i = 0; i < numPrograms; i++)
+            {
+                panel1.Controls.RemoveByKey("programLabelID" + i);
+                panel1.Controls.RemoveByKey("programDeleteButtonID" + i);
+            }
+            numPrograms = 0;
+            string lastTimeRun = "Never";
+            string Name;
+            foreach (TrackedProgram item in Programs)
+            {
+                int id = numPrograms;
+                Button programDeleteButton = new Button();
+                programDeleteButton.Size = new Size(96, 37);
+                programDeleteButton.Text = "Delete Program";
+                programDeleteButton.Location = new Point(430, (id * 100) + 46);
+                programDeleteButton.Name = "programDeleteButtonID" + id;
+                programDeleteButton.Click += new EventHandler((sender, e) => ProgramDeleteButton_Click(sender, e, id));
+
+                Label programLabel = new Label();
+                programLabel.Location = new Point(0, (id * 100) + 12);
+                programLabel.Font = new Font(programLabel.Font.FontFamily, 10);
+                programLabel.AutoSize = true;
+                programLabel.Name = "programLabelID" + id;
+
+                string s = "";
+                string Path = "";
+                var days = TimeSpan.FromMinutes(item.runtime).Days;
+                var hours = TimeSpan.FromMinutes(item.runtime).Hours;
+                var minutes = TimeSpan.FromMinutes(item.runtime).Minutes;
+                if (item.lastTimeRun != DateTime.MinValue) lastTimeRun = item.lastTimeRun.ToString("F");
+                if (processIsRunning(item.Path)) Name = item.Name + " (Currently running)";
+                else Name = item.Name;
+                if (showPaths==true)Path = "\nPath: " + item.Path;
+                s =
+                    s +
+                    "Name: " +
+                    Name + "    [ID: " + numPrograms + "]" +
+                    "\nActive run time: " +
+                    String.Format("{0} Days, {1} Hours, {2} Minutes", days, hours, minutes) +
+                    "\nLast time run: " +
+                    lastTimeRun +
+                    "\nTime added: " +
+                    item.timeAdded.ToString("F") + Path;
+
+                programLabel.Text = s;
+                panel1.Controls.Add(programLabel);
+                panel1.Controls.Add(programDeleteButton);
+                programDeleteButton.BringToFront();
+                
+                numPrograms++;
+            }
+            label1.Text = "Current number of tracked programs: " + numPrograms;
+        }
+
+        private void ProgramDeleteButton_Click(object sender, EventArgs e, int id)
+        {
+            DialogResult dialogResult = MessageBox.Show("Do you really want to remove this program?", "Delete Program", MessageBoxButtons.YesNo);
+            if(dialogResult == DialogResult.Yes)
+            {
+                deleteProgram(id);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "SQLite Databank (*.sqlite)|*.sqlite|All files (*.*)|*.*";
+            sfd.FileName = "AppTime.sqlite";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                dbLoc = sfd.FileName;
+                using (RegistryKey Key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\sinnzrAppTime\", true))
+                    if (Key != null) //SubKey already exists
+                    {
+                        SQLiteConnection.CreateFile(sfd.FileName);
+                        Key.SetValue("databaseLocation", sfd.FileName);
+                        dbExist = true;
+                        databaseAvailableLabel.Visible = false;
+                        connectDb(true, true);
+                    }
+                    else //create key
+                    {
+                        SQLiteConnection.CreateFile(sfd.FileName);
+                        using (RegistryKey SubKey = Registry.CurrentUser.CreateSubKey(@"SOFTWARE\sinnzrAppTime"))
+                            SubKey.SetValue("databaseLocation", sfd.FileName);
+                        dbExist = true;
+                        databaseAvailableLabel.Visible = false;
+                        connectDb(true, true);
+                    }
+            }
+        }
+
+        private void connectDb(bool firstTime, bool receiveSuccessMessage)
+        {
+            if (dbConnected) return;
+            if (dbExist)
+            {
+                dbConnection = new SQLiteConnection("Data Source=" + dbLoc + ";Version=3;");
+                dbConnection.Open();
+                dbConnected = true;
+                if (firstTime)
+                {
+                    ExecuteQuery(@"CREATE TABLE programs (name VARCHAR(128), path VARCHAR(128), runtime INT default 0, lastTimeRun DATETIME default null, timeAdded DATETIME default CURRENT_TIMESTAMP);
+                                   CREATE TABLE `config` (`settingkey`	VARCHAR(128),`settingstate`	INT DEFAULT null,`settingvalue`	VARCHAR(128) DEFAULT null);
+                                   INSERT INTO config (settingkey, settingstate) VALUES ('showPaths', 0)");
+                }
+                if (receiveSuccessMessage) MessageBox.Show("Sucessfully connected to SQLite database.");
+            }
+            else MessageBox.Show("Database doesn't exist, unable to connect.");
+        }
+
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (dbConnected)
+            {
+                updatePrograms(false, true);
+                dbConnection.Close();
+            }
+            else return;
+        }
+
+        private void AddProgram(string path, string name)
+        {
+            SQLiteCommand cmd = new SQLiteCommand("insert into programs (name, path, timeAdded) VALUES (@name, @path, @timeAdded);", dbConnection);
+            cmd.Parameters.AddWithValue("@name", name);
+            cmd.Parameters.AddWithValue("@path", path);
+            cmd.Parameters.AddWithValue("@timeAdded", DateTime.Now);
+            cmd.ExecuteNonQuery();
+            TrackedProgram p = new TrackedProgram(path, name, 0);
+            Programs.Add(p);
+            updateLabel();
+        }
+
+        private void updatePrograms(bool receiveSuccessMessage, bool automatic)
+        {
+            if (dbConnected)
+            {
+                foreach (TrackedProgram item in Programs)
+                {
+                    SQLiteCommand cmd = new SQLiteCommand("UPDATE programs SET runtime = @runtime, lastTimeRun = @lastTimeRun WHERE name = @name AND path = @path", dbConnection);
+                    cmd.Parameters.AddWithValue("@runtime", item.runtime);
+                    cmd.Parameters.AddWithValue("@lastTimeRun", item.lastTimeRun);
+                    cmd.Parameters.AddWithValue("@name", item.Name);
+                    cmd.Parameters.AddWithValue("@path", item.Path);
+                    cmd.ExecuteNonQuery();
+                }
+                if (receiveSuccessMessage) MessageBox.Show("Successfully updated database.");
+                if (automatic) label3.Text = "Last updated: " + DateTime.Now.ToString("F") + " (Automatic update)";
+                else {
+                    label3.Text = "Last updated: " + DateTime.Now.ToString("F");
+                    automaticUpdateTimer.Stop();
+                    automaticUpdateTimer.Start();
+                }
+            }
+            else MessageBox.Show("Can't update programs: not connected to a database.");
+        }
+
+        private void loadDatabase()
+        {
+            if (dbConnected)
+            {
+                string stm = "select * from programs";
+                using (SQLiteCommand cmd = new SQLiteCommand(stm, dbConnection))
+                {
+                    using (SQLiteDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            TrackedProgram tp = new TrackedProgram();
+                            tp.Path = reader["path"].ToString();
+                            tp.Name = reader["name"].ToString();
+                            tp.runtime = (int)reader["runtime"];
+                            tp.lastTimeRun = reader["lastTimeRun"] as DateTime? ?? default(DateTime);
+                            tp.timeAdded = (DateTime)reader["timeAdded"];
+                            Programs.Add(tp);
+                        }
+                    }
+                }
+                updateLabel();
+            }
+            else MessageBox.Show("Can't load programs: not connected to a database.");
+        }
+
+        private void timeTrackTimer_Tick(object sender, EventArgs e)
+        {
+            foreach (TrackedProgram item in Programs)
+            {
+                if (processIsRunning(item.Path))
+                {
+                    item.runtime++;
+                    item.lastTimeRun = DateTime.Now;
+                }
+            }
+            updateLabel();
+        }
+
+        private void ExecuteQuery(string query)
+        {
+            if (dbConnected)
+            {
+                SQLiteCommand command = new SQLiteCommand(query, dbConnection);
+                command.ExecuteNonQuery();
+            }
+            else MessageBox.Show("Error executing query: not connected to database.");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            updatePrograms(true, false);
+            updateSettings();
+        }
+
+        private bool processIsRunning(string path)
+        {
+            Process[] processes = Process.GetProcessesByName(Path.GetFileNameWithoutExtension(path));
+            if (processes.Length > 0) return true;
+            else return false;
+        }
+
+        private void automaticUpdateTimer_Tick(object sender, EventArgs e)
+        {
+            updatePrograms(false, true);
+        }
+        
+        private void deleteProgram(int id)
+        {
+            if (dbConnected)
+            {
+                SQLiteCommand cmd = new SQLiteCommand("DELETE FROM programs WHERE name=@name AND path=@path", dbConnection);
+                cmd.Parameters.AddWithValue("@name", Programs[id].Name);
+                cmd.Parameters.AddWithValue("@path", Programs[id].Path);
+                cmd.ExecuteNonQuery();
+                Programs.RemoveAt(id);
+                updateLabel();
+                MessageBox.Show("Successfully deleted Program.");
+            }
+            else MessageBox.Show("Can't delete program: Not connected to database.");
+        }
+
+        public void updateSettings()
+        {
+            if (dbConnected)
+            {
+                int showPathsState;
+                SQLiteCommand cmd = new SQLiteCommand("UPDATE config SET settingstate=@settingstate WHERE settingkey='showPaths';", dbConnection);
+                if (showPaths) showPathsState = 1;
+                else showPathsState = 0;
+                cmd.Parameters.AddWithValue("@settingstate", showPathsState);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void loadSettings()
+        {
+            if (dbConnected)
+            {
+                SQLiteCommand cmd = new SQLiteCommand("SELECT settingstate FROM config WHERE settingkey='showPaths'", dbConnection);
+                if (Convert.ToInt32(cmd.ExecuteScalar()) == 0) { this.showPaths = false; checkBox1.Checked = false; }
+                else { this.showPaths = true; checkBox1.Checked = true; }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (settingsPanelExtended)
+            {
+                panel2.Enabled = false;
+                this.Size = new Size(601, 499);
+                settingsPanelExtended = false;
+            }
+            else {
+                panel2.Enabled = true;
+                this.Size = new Size(752, 499);
+                settingsPanelExtended = true;
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            showPaths = checkBox1.Checked;
+            updateLabel();
+        }
+    }
+}
