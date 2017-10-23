3bc1429f8adeb7f5d33918e3df67bfa05530a9d1
Sinner <sinn3r@live.de>
2017-10-23 20:28:15 +0200

    Fixed process list

--- ProcessForm.Designer.cs
+++ ProcessForm.Designer.cs
@@ -84,6 +84,7 @@
             // 
             // button1
             // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.button1.Location = new System.Drawing.Point(12, 300);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(250, 23);
@@ -94,6 +95,7 @@
             // 
             // button2
             // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.button2.Location = new System.Drawing.Point(268, 300);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(250, 23);
